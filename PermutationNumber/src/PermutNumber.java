89519d24b51662832b6f03080f776ad44ce991bc
Evgen <evgenlubich4@gmail.com>
2014-11-30 14:12:13 +0200

    необходимо реализация сортировки, перезапись массива

--- PermutationNumber/src/PermutNumber.java
+++ PermutationNumber/src/PermutNumber.java
@@ -5,20 +5,41 @@ import java.util.Arrays;
  */
 public class PermutNumber {
     public static void main(String[] args) {
-        /*for (int i = 0; i<100;i++){
-            if () {
-                System.out.println(i);
-            }
-        } */
+        for (int i = 0; i < 3; i++) {
+            int elem = numberOfEl(i);
+            System.out.println("elem = " + elem);
+            int mass[] = new int[elem];
+            System.out.println("mass length = " + mass.length);
 
-        int x = 123456;
-        //int sum = 0;
-        int mass [] = new int[7];
-        for (int i=0;i>5;i++){
-           int sum = x % 10;
-            mass[i]=sum;
-            x /= 10;
+            for (int k = 0; k <= mass.length; k++) {
+                int sum = i % 10;
+
+
+                mass = new int[elem];
+                System.out.println("mass length = " + mass.length);
+                mass[i] = sum;
+                i /= 10;
+                //if () {
+                System.out.println();
+
+                //}
+            }
+            System.out.println(Arrays.toString(mass));
+
         }
-        System.out.println(Arrays.toString(mass));
+//        System.out.println(Arrays.toString(mass));
+    }
+
+
+    public static int numberOfEl(int count) {
+        int a = count;
+        int countElem = 0;
+        while (a > 0) {
+            int sum = a % 10;
+            countElem++;
+            a /= 10;
+        }
+        //System.out.println("count Elem = " + countElem);
+        return countElem;
     }
 }
